588d4aa1298542eff09567d17d3f1cf3afd71271
krypton-byte <rosid6434@gmail.com>
2022-02-10 20:57:12 +0700

    footer

--- user.php
+++ user.php
@@ -136,11 +136,17 @@
     </div>
 
     <footer class="main-footer">
-      <div class="float-right d-none d-sm-block">
-        <b>UKK RPL 2022</b> Aplikasi Peduli Diri
-      </div>
-      <strong>Copyright &copy; 2022 <a href="https://rpl.smkn1panyingkiran.sch.id">RPL SMKN 1 Panyingkiran</a>.</strong> Nama Peserta.
-    </footer>
+  <div class="float-right d-none d-sm-block">
+    <b>UKK RPL 2022</b> Aplikasi Peduli Diri
+  </div>
+  <strong
+    >Copyright © <?php echo date('Y')?>
+    <a href="https://rpl.smkn1panyingkiran.sch.id"
+      >RPL SMKN 1 Panyingkiran</a
+    >.</strong
+  >
+  <a style="color:gray;" href="https://github.com/krypton-byte" target="_blank" rel="noopener noreferrer">Puja</a>
+</footer>
 
   </div>
 
