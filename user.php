9bb93ae5a2a0a74168cf86423181d9b2a7b97d7f
krypton-byte <rosid6434@gmail.com>
2022-02-16 18:33:32 +0700

    fix: catatan

--- user.php
+++ user.php
@@ -107,7 +107,7 @@
         <div class="container-fluid">
           <div class="row mb-2">
             <div class="col-sm-6">
-              <h1>Catatan Perjalanan <?php $_SESSION['nama'] ?></h1>
+              <h1>Catatan Perjalanan <?php echo htmlspecialchars($_SESSION['nama']) ?></h1>
             </div>
           </div>
         </div>
